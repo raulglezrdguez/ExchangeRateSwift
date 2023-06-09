8ea327c432f20398d75ddc4e3141a523c4b87a7f
raulglezrdguez <raulglezrdguez69@gmail.com>
2023-06-09 14:11:31 -0400

    Initial Commit

---
+++ ExchangeRateSwift/ExchangeRateSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRateSwiftApp.swift
+//  ExchangeRateSwift
+//
+//  Created by Raul Glez Rdguez on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRateSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
